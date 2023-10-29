612ed9db275100731e5a4b86c832bac22ac93c4f
Joey-Liao <630843901@qq.com>
2023-10-29 17:27:17 +0800

    Initial commit

---
+++ src/main/java/com/lzy/mfsse/MfsseApplication.java
@@ -0,0 +1,13 @@
+package com.lzy.mfsse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfsseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfsseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzy/mfsse/MfsseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzy.mfsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfsseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
